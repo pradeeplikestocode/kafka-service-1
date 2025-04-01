627c5c21e76c94506a5448359b15855175921c7b
Pradeep Kumar Rai <pradrai@Pradeeps-MacBook-Air.local>
2025-04-01 09:00:19 +0530

    first commit

---
+++ src/main/java/com/pradrai/kafka_server_1/KafkaServer1Application.java
@@ -0,0 +1,13 @@
+package com.pradrai.kafka_server_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaServer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradrai/kafka_server_1/KafkaServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradrai.kafka_server_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
